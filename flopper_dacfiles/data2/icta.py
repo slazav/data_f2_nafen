4fc8c6fd1be3048bb16a68dc949460da27208a21
Vladislav Zavjalov <slazav@altlinux.org>
2020-04-21 23:57:12 +0100

    flopper_dacfiles/data_freq2: add icta to each point

--- flopper_dacfiles/data2/icta.py
+++ flopper_dacfiles/data2/icta.py
@@ -8,18 +8,12 @@ from non_uniform_savgol import non_uniform_savgol
 import matplotlib.pyplot as plt
 
 ################################
-# Get smoothed ICTA data for a given time array
-# Optionaly make a control plot
+# Get smoothed ICTA data for a given time range
 
 # times - numpy array with times
 # win  - Smoothing window, points. Default: -1 (no smoothing)
 # marg - Time margins, s. Default 600
-# return: numpy array of same size at `times`
-def get_icta(times, win=0, marg=600, plot=''):
-
-  # load ICTA data for the whole time range
-  tmin = numpy.min(times)
-  tmax = numpy.max(times)
+def get_icta(tmin, tmax, win=0, marg=600):
 
   r = requests.get(url = "http://slazav.xyz:8091/get_prev?name=icta&t1=%f" %(tmin-marg))
   if r.text=="": t1=tmin-marg
@@ -49,6 +43,26 @@ def get_icta(times, win=0, marg=600, plot=''):
     icta_st = icta_t
     icta_sw = icta_w
 
+
+  return (icta_t, icta_w, icta_st, icta_sw)
+
+
+################################
+# Interpolate ICTA data
+# Optionaly make a control plot
+
+# times - numpy array with times
+# win  - Smoothing window, points. Default: -1 (no smoothing)
+# marg - Time margins, s. Default 600
+# return: numpy array of same size at `times`
+def interp_icta(times, win=0, marg=600, plot=''):
+
+  # load ICTA data for the whole time range
+  tmin = numpy.min(times)
+  tmax = numpy.max(times)
+
+  (icta_t, icta_w, icta_st, icta_sw) = get_icta(tmin, tmax, win, marg)
+
   # interpolate to data values
   icta = numpy.interp(times, icta_st, icta_sw)
 
@@ -62,5 +76,6 @@ def get_icta(times, win=0, marg=600, plot=''):
     plt.ylabel('ICTA width, Hz')
     plt.legend()
     plt.savefig(plot)
+
 
   return icta

67425a3190f149291b4dc30f443d75897f8ad770
Vladislav Zavjalov <slazav@altlinux.org>
2020-04-22 15:44:51 +0100

    non_lin_osc: remove old matlab code

--- non_lin_osc/osc_any/nonlin_osc.m
+++
@@ -1,8 +0,0 @@
-% find equilibrium (zero of nonlin_osc_eq function)
-function uv = nonlin_osc(w,func, uv0)
-  for i=1:length(w)
-    f = @(uv) nonlin_osc_eq(uv,w(i),func);
-    uv0 = fsolve(f, uv0);
-    uv(i,:) = uv0;
-  end
-end
--- non_lin_osc/osc_pp/x_harm.m
+++
@@ -1,5 +0,0 @@
-% equilibrium for harmonic oscillator, use for initial conditions
-function [x y] = x_harm(w,w0,d,g)
-  x = - (w.^2-w0^2)*g./((w.^2-w0^2).^2 + (w*d).^2);
-  y = - w*d*g./((w.^2-w0^2).^2 + (w*d).^2);
-end

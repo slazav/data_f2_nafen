38808c966f5842013d2d81c1f6d7c33fb7f0bc8e
Vladislav Zavjalov <slazav@altlinux.org>
2020-04-22 13:14:22 +0100

    solve_nonlin: fix integration; add test for duffing oscillator

--- non_lin_osc/solve_nonlin/non_lin_osc.py
+++ non_lin_osc/solve_nonlin/non_lin_osc.py
@@ -12,7 +12,7 @@ import scipy.integrate
 #
 # u,v - van der Pol coordinates (x,x' rotated by w*t)
 # w - frequency
-# func - second derivative function, x'' = func(x, x')
+# func - second derivative function, x'' = -func(x, x')
 #
 # Coordinated are averaged over rotation period (phase 0..2*pi)
 # Output is u' and v',  they should be zero in equilibrium.
@@ -21,18 +21,17 @@ import scipy.integrate
 #
 
 def nonlin_osc_eq(uv,w, F, func, fpars):
-
   p = 2*math.pi*numpy.linspace(0,1,100) # phase for integration
   sp = numpy.sin(p)
   cp = numpy.cos(p)
+
   x = uv[0]*cp-uv[1]*sp;
+
   dx = w*(-uv[0]*sp-uv[1]*cp);
   ddx = F*cp - func(x,dx,fpars);
 
-  duv = [-scipy.integrate.trapz(p, (ddx + w**2*x)*sp)/w,\
-         -scipy.integrate.trapz(p, (ddx + w**2*x)*cp)/w];
-  return duv
-
+  return [-scipy.integrate.trapz((ddx + w**2*x)*sp, p)/w,\
+          -scipy.integrate.trapz((ddx + w**2*x)*cp, p)/w];
 
 #######################################################
 
@@ -53,3 +52,23 @@ def osc_pseudopl1(x,dx, pars):
   vc  = pars[2] # critical velocity
   k   = pars[3] # relaxation factor for large velocity
   return w0**2*x + dx/tau * (k - (1-k) * vc/numpy.sqrt(vc**2 + dx**2))
+
+## duffing osc
+def osc_duffing(x,dx, pars):
+  w0  = pars[0] # resonant frequency
+  tau = pars[1] # relaxation time at low velocuty
+  a   = pars[2] # non-linear parameter
+  return w0**2*x + dx/tau + a*x**3
+
+#######################################################
+
+# Equilibriun function and solver for Duffung oscillator at periodic drive.
+# see http://www.scholarpedia.org/article/Duffing_oscillator
+# Note that solver does not work when hysteresis appears.
+
+def nonlin_osc_eq_duff(uv, w, F, w0, tau, a):
+  return [(-w*uv[0]/tau + (w**2-w0**2)*uv[1] - 3/4.0*a*(uv[1]**2+uv[0]**2)*uv[1])/2/w,\
+          (-w*uv[1]/tau - (w**2-w0**2)*uv[0] + 3/4.0*a*(uv[1]**2+uv[0]**2)*uv[0] - F)/2/w]
+
+def nonlin_osc_solve_duff(w, F, w0, tau, a, uv0):
+  return scipy.optimize.fsolve(nonlin_osc_eq_duff, uv0, (w,F,w0, tau, a))
